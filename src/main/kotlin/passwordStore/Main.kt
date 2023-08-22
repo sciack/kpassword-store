f6658d8c83d2b5f8250eccbb79a4066d44666325
Mirko Sciachero <m.sciachero@gmail.com>
2023-08-22 19:08:47 +0800

    fix: redesign the code menu, so each item is a single function

--- src/main/kotlin/passwordStore/Main.kt
+++ src/main/kotlin/passwordStore/Main.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Density
@@ -27,7 +26,6 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import com.zaxxer.hikari.HikariDataSource
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
@@ -39,26 +37,16 @@ import org.kodein.di.compose.rememberInstance
 import org.kodein.di.compose.withDI
 import org.kodein.di.instance
 import org.slf4j.bridge.SLF4JBridgeHandler
-import passwordStore.config.SetupEnv
 import passwordStore.config.configureEnvironment
 import passwordStore.config.getMode
 import passwordStore.navigation.NavController
 import passwordStore.navigation.rememberNavController
 import passwordStore.services.download
-import passwordStore.services.exportPath
-import passwordStore.services.performDownload
 import passwordStore.services.upload
 import passwordStore.sql.Migration
 import passwordStore.users.UserVM
-import passwordStore.utils.Platform
 import passwordStore.utils.StatusHolder
-import java.nio.file.Files
-import java.nio.file.Path
 import javax.sql.DataSource
-import javax.swing.UIManager
-import kotlin.io.path.createDirectories
-import kotlin.io.path.notExists
-import kotlin.io.path.writer
 import kotlin.system.exitProcess
 
 @Composable
@@ -155,191 +143,164 @@ fun customShape() = object : Shape {
 }
 
 @Composable
+fun RowScope.MenuItem(
+    icon: @Composable () -> Unit,
+    title: String,
+    testTag: String = "",
+    onClick: () -> Unit
+) {
+    IconButton(
+        onClick = onClick,
+        modifier = Modifier.testTag(testTag).align(Alignment.CenterVertically).size(24.dp)
+    ) {
+        icon()
+    }
+    Text(
+        text = title,
+        style = MaterialTheme.typography.body1,
+        modifier = Modifier.clickable {
+            onClick()
+        }.align(Alignment.CenterVertically)
+    )
+}
+
+
+@Composable
 fun drawer(navController: NavController) {
     val di: DI = localDI()
     val coroutineScope = rememberCoroutineScope()
     val userVM by rememberInstance<UserVM>()
 
     Row {
-
-        IconButton(
+        MenuItem(
             onClick = { navController.navigate(Screen.List) },
-            modifier = Modifier.testTag("Home").align(Alignment.CenterVertically).size(24.dp)
-        ) {
-            Icon(
-                Icons.Default.Home, contentDescription = Screen.List.name,
-            )
-        }
-        Text(
-            text = Screen.List.name,
-            style = MaterialTheme.typography.body1,
-            modifier = Modifier.clickable {
-                navController.navigate(Screen.List)
-            }.align(Alignment.CenterVertically)
-        )
-    }
-    Spacer(Modifier.height(12.dp))
-    Row {
-        IconButton(
-            onClick = {
-                navController.navigate(Screen.NewService)
-            },
-            modifier = Modifier.testTag("New Service").align(Alignment.CenterVertically).size(24.dp)
-        ) {
-            Icon(
-                Icons.Default.Create, contentDescription = Screen.NewService.name,
-
+            title = Screen.List.name,
+            testTag = "Home",
+            icon = {
+                Icon(
+                    Icons.Default.Home, contentDescription = Screen.List.name,
                 )
-        }
-        Text(
-            text = Screen.NewService.name,
-            style = MaterialTheme.typography.body1,
-            modifier = Modifier.clickable {
-                navController.navigate(Screen.NewService)
-            }.align(Alignment.CenterVertically)
+            }
         )
     }
     Spacer(Modifier.height(12.dp))
     Row {
-        IconButton(
+        MenuItem(
+            onClick = { navController.navigate(Screen.NewService) },
+            title = Screen.NewService.name,
+            testTag = "New Service",
+            icon = {
+                Icon(
+                    Icons.Default.Create, contentDescription = Screen.NewService.name,
+                )
+            }
+        )
+    }
+    Spacer(Modifier.height(12.dp))
+    Row {
+        MenuItem(
             onClick = { navController.navigate(Screen.History) },
-            modifier = Modifier.align(Alignment.CenterVertically).size(24.dp)
-        ) {
-
-            Icon(
-                painterResource("/icons/history.svg"),
-                contentDescription = Screen.History.name
-            )
-        }
-        Text(
-            text = Screen.History.name,
-            style = MaterialTheme.typography.body1,
-            modifier = Modifier.clickable {
-                navController.navigate(Screen.History)
-            }.align(Alignment.CenterVertically)
+            title = Screen.History.name,
+            testTag = "History",
+            icon = {
+                Icon(
+                    painterResource("/icons/history.svg"),
+                    contentDescription = Screen.History.name
+                )
+            }
         )
     }
     Spacer(Modifier.height(12.dp))
     Divider(color = Color.LightGray, thickness = 1.dp)
     Spacer(Modifier.height(12.dp))
     Row {
-        IconButton(
+        MenuItem(
             onClick = {
                 coroutineScope.download(di)
             },
-            modifier = Modifier.align(Alignment.CenterVertically).size(24.dp)
-        ) {
-
-            Icon(
-                painterResource("/icons/file_csv.svg"),
-                contentDescription = "Export CSV"
-            )
-        }
-        Text(
-            text = "Export CSV",
-            style = MaterialTheme.typography.body1,
-            modifier = Modifier.clickable {
-                coroutineScope.download(di)
-            }.align(Alignment.CenterVertically)
+            title = "Export CSV",
+            testTag = "ExportCsv",
+            icon = {
+                Icon(
+                    painterResource("/icons/file_csv.svg"),
+                    contentDescription = "Export CSV"
+                )
+            }
         )
+
     }
     Spacer(Modifier.height(12.dp))
     Row {
-        IconButton(
+        MenuItem(
             onClick = {
                 coroutineScope.launch {
                     upload(di)
                 }
             },
-            modifier = Modifier.align(Alignment.CenterVertically).size(24.dp)
-        ) {
-
-            Icon(
-                painterResource("/icons/upload.svg"),
-                contentDescription = "Import CSV"
-            )
-        }
-        Text(
-            text = "Import CSV",
-            style = MaterialTheme.typography.body1,
-            modifier = Modifier.clickable {
-                coroutineScope.launch {
-                    upload(di)
-                }
-            }.align(Alignment.CenterVertically)
+            title = "Import CSV",
+            testTag = "ImportCsv",
+            icon = {
+                Icon(
+                    painterResource("/icons/upload.svg"),
+                    contentDescription = "Import CSV"
+                )
+            }
         )
     }
     Spacer(Modifier.height(12.dp))
     Divider(color = Color.LightGray, thickness = 1.dp)
     Spacer(Modifier.height(12.dp))
     Row {
-        IconButton(
+        MenuItem(
             onClick = {
                 navController.navigate(Screen.Users)
             },
-            modifier = Modifier.align(Alignment.CenterVertically).size(24.dp)
-        ) {
-            Icon(
-                Icons.Default.AccountCircle,
-                "Users"
-            )
-        }
-        Text(
-            text = "Users",
-            style = MaterialTheme.typography.body1,
-            modifier = Modifier.clickable {
-                navController.navigate(Screen.Users)
-            }.align(Alignment.CenterVertically)
+            title = "Users",
+            testTag = "Users",
+            icon = {
+                Icon(
+                    painterResource("/icons/file_csv.svg"),
+                    contentDescription = "Users"
+                )
+            }
         )
     }
     Spacer(Modifier.height(12.dp))
     if (userVM.loggedUser.value.isAdmin()) {
         Row {
-            IconButton(
+            MenuItem(
                 onClick = {
-                    navController.navigate(Screen.Users)
-                },
-                modifier = Modifier.align(Alignment.CenterVertically).size(24.dp)
-            ) {
-                Icon(
-                    Icons.Default.AccountBox,
-                    "Users"
-                )
-            }
-            Text(
-                text = "Create User",
-                style = MaterialTheme.typography.body1,
-                modifier = Modifier.clickable {
                     navController.navigate(Screen.CreateUser)
-                }.align(Alignment.CenterVertically)
+                },
+                title = "Create User",
+                testTag = "CreateUser",
+                icon = {
+                    Icon(
+                        Icons.Default.AccountBox,
+                        "CreateUsers"
+                    )
+                }
             )
         }
     }
     Divider(color = Color.LightGray, thickness = 1.dp)
     Spacer(Modifier.height(12.dp))
     Row {
-        IconButton(
+        MenuItem(
             onClick = {
-               exitProcess(0)
-            },
-            modifier = Modifier.align(Alignment.CenterVertically).size(24.dp)
-        ) {
-            Icon(
-                Icons.Default.ExitToApp,
-                "Exit"
-            )
-        }
-        Text(
-            text = "Exit",
-            style = MaterialTheme.typography.body1,
-            modifier = Modifier.clickable {
                 exitProcess(0)
-            }.align(Alignment.CenterVertically)
+            },
+            title = "Exit",
+            testTag = "Exit",
+            icon = {
+                Icon(
+                    Icons.Default.ExitToApp,
+                    "Exit"
+                )
+            }
         )
     }
 }
-
-
 
 
 private fun configureLog(): KLogger {
